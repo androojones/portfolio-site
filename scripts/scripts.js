b1094daa6b3948f95742bfc725bf4069a5d4b495
Andrew Jones <15722138+androojones@users.noreply.github.com>
2019-04-09 12:49:11 -0300

    Initial Project Upload
    Completed site; projects need updating on rolling basis.

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+new fullpage('#fullpage', {
+  //options here
+  autoScrolling:true,
+  // scrollHorizontally: false,
+  licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+  navigation: true,
+  navigationPosition: 'right',
+  navigationTooltips: ['Home', 'Work', 'About', 'Contact'],
+  scrollOverflow: true,
+  responsiveWidth: 750,
+  responsiveHeight: 600
+});
